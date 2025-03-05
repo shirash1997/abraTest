de3f69e0096bf9b473d4c2b37360bff9e8fc18e4
shirash1997! <shira.shatz@abra-it.com>
2025-03-05 18:10:51 +0200

    static resource styles +menue component css fix

--- force-app/main/default/lwc/menuComponent/menuComponent.js
+++ force-app/main/default/lwc/menuComponent/menuComponent.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import Styles from '@salesforce/resourceUrl/Styles';
 
-export default class MenuComponent extends LightningElement {}
+export default class MenuComponent extends LightningElement {
+
+    connectedCallback(){
+        loadStyle(this, Styles);
+    }
+}
