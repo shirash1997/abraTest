2e85c828ce13de824ac1ba4d6ecd7313c8640548
shirash1997! <shira.shatz@abra-it.com>
2025-03-16 08:01:12 +0200

    change all structure

--- force-app/main/default/lwc/menuComponent/menuComponent.js
+++ force-app/main/default/lwc/menuComponent/menuComponent.js
@@ -1,10 +1,17 @@
 import { LightningElement } from 'lwc';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import Styles from '@salesforce/resourceUrl/Styles';
+import logo from '@salesforce/resourceUrl/MidnightBrewLogo';
 
 export default class MenuComponent extends LightningElement {
-
+    logoUrl = logo;
+    
     connectedCallback(){
         loadStyle(this, Styles);
     }
+
+    navigateToHome() {
+        this.dispatchEvent(new CustomEvent('navigation', { detail: { page: 'home' } }));
+    }
+
 }
