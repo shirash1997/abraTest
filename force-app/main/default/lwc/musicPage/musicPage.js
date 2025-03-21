d2403c5b316af052d96a9622d2121ec5ec7c4048
shirash1997! <shira.shatz@abra-it.com>
2025-03-21 13:57:24 +0200

    music page finished

--- force-app/main/default/lwc/musicPage/musicPage.js
+++ force-app/main/default/lwc/musicPage/musicPage.js
@@ -1,8 +1,8 @@
 import { LightningElement } from 'lwc';
-import performancesVideoResource from '@salesforce/resourceUrl/performancesVideo';
+import musicVideo from '@salesforce/resourceUrl/musicVideo';
 
 export default class MusicPage extends LightningElement {
-    videoUrl = `${performancesVideoResource}/performancesVideo/video.html`;
+    videoUrl = `${musicVideo}/video.html`;
 
     connectedCallback(){
             console.log('video url in music:'+`${this.videoUrl}`);
