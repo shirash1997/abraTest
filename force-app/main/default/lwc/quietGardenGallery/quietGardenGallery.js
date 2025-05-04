0c7e019468acdb6f53e3ec634ab2c490e98ea15c
shirash1997! <shira.shatz@abra-it.com>
2025-05-04 10:23:40 +0300

    fixes

--- force-app/main/default/lwc/quietGardenGallery/quietGardenGallery.js
+++ force-app/main/default/lwc/quietGardenGallery/quietGardenGallery.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import quietGardenGallery from '@salesforce/resourceUrl/quietGardenGallery';
 
 export default class QuietGardenGallery extends LightningElement {
@@ -7,29 +7,42 @@ export default class QuietGardenGallery extends LightningElement {
   img3 = `${quietGardenGallery}/GalleryGarden_Optimized/img3.jpg`;
   @track isLightboxOpen = false;
   @track lightboxImage;
+  @track isModalOpen = false;
 
-  openLightbox1() {
-    this.lightboxImage = this.img1;
-    this.isLightboxOpen = true;
+  renderedCallback() {
+    const dateInput = this.template.querySelector('input[type="date"]');
+    const timeInput = this.template.querySelector('input[type="time"]');
+
+    if (dateInput) {
+        dateInput.setAttribute('lang', 'he');
+        dateInput.setAttribute('dir', 'rtl');
+    }
+    if (timeInput) {
+        timeInput.setAttribute('lang', 'he');
+        timeInput.setAttribute('dir', 'rtl');
+    }
+    }
+  openLightbox1() { this.lightboxImage = this.img1; this.isLightboxOpen = true; }
+  openLightbox2() { this.lightboxImage = this.img2; this.isLightboxOpen = true; }
+  openLightbox3() { this.lightboxImage = this.img3; this.isLightboxOpen = true; }
+
+  closeLightbox() { this.isLightboxOpen = false; this.lightboxImage = null; }
+
+  openReservationModal() {
+    this.isModalOpen = true;
   }
 
-  openLightbox2() {
-    this.lightboxImage = this.img2;
-    this.isLightboxOpen = true;
-  }
-
-  openLightbox3() {
-    this.lightboxImage = this.img3;
-    this.isLightboxOpen = true;
-  }
-
-  closeLightbox() {
-    this.isLightboxOpen = false;
-    this.lightboxImage = null;
+  closeModal() {
+    this.isModalOpen = false;
   }
 
   stopPropagation(event) {
     event.stopPropagation();
   }
-}
 
+  handleSubmit(event) {
+    event.preventDefault();
+    alert('ההזמנה נשלחה! נשמח לראותך 😊');
+    this.isModalOpen = false;
+  }
+}
